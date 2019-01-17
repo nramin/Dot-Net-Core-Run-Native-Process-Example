74bc83684711f10ac24ae7013ca3e826c6ba0a88
Ramin <ramin@localhost.localdomain>
2019-01-17 15:11:41 -0800

    Setting up repository

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Core 
+{
+    class Program 
+    {
+        static void Main (string[] args)
+        {
+            const String ListCommand = "-c ls";
+
+            Console.Write("Enter a path to get a list of its files: ");
+            String path = Console.ReadLine();
+            Console.WriteLine(String.Format("Command to be executed on bash: {0} {1}", ListCommand, path));
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    WorkingDirectory = @path,
+                    Arguments = ListCommand,
+                    FileName = "/bin/bash",
+                    RedirectStandardOutput = true
+                }
+            };
+            process.Start();
+            Console.WriteLine(process.StandardOutput.ReadToEnd());
+            process.WaitForExit();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Dot Net Core Run Native Process Example
+An example of running a native unix command (ls) under Dot Net Core in C#.
+
+To run:
+1. Navigate to this project's folder
+2. dotnet restore
+3. dotnet run
